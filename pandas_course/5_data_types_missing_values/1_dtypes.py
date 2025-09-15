d23026afdfaf8f1e7941899e6ab23b339d208333
MateusSM2000 <matt_petao@hotmail.com>
2025-09-15 15:23:45 -0300

    update

--- pandas_course/5_data_types_missing_values/1_dtypes.py
+++ pandas_course/5_data_types_missing_values/1_dtypes.py
@@ -2,4 +2,10 @@ import pandas as pd
 reviews = pd.read_csv('../winemag-data-130k-v2.csv', index_col=0)
 
 
-print(reviews.price.dtype, type(reviews.price.dtype), '\n' * 10)
+print(reviews.price.dtype, type(reviews.price.dtype), '\n')
+print(reviews.dtypes, type(reviews.dtypes), '\n' * 10)
+
+print(reviews.points.dtype, '\n')
+print(reviews.points.astype('float64'), '\n' * 10)
+
+print(reviews.index.dtype)
