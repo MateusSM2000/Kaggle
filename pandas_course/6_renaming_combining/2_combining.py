e22c1452301f9aec7f23b8746c18f382f55c67d3
MateusSM2000 <matt_petao@hotmail.com>
2025-09-16 12:34:00 -0300

    update

--- pandas_course/6_renaming_combining/2_combining.py
+++ pandas_course/6_renaming_combining/2_combining.py
@@ -1,3 +1,5 @@
+from copy import deepcopy
+
 import pandas as pd
 ca_videos = pd.read_csv('../CAvideos.csv')
 gb_videos = pd.read_csv('../GBvideos.csv')
@@ -5,8 +7,11 @@ gb_videos = pd.read_csv('../GBvideos.csv')
 print(ca_videos, '\n')
 print(gb_videos, '\n' * 10)
 
-
-ca_gb_videos = pd.concat([ca_videos, gb_videos])
+ca_videos_2 = deepcopy(ca_videos)
+ca_videos_2['country'] = 'Canada'
+gb_videos_2 = deepcopy(gb_videos)
+gb_videos_2['country'] = 'United Kingdom'
+ca_gb_videos = pd.concat([ca_videos_2, gb_videos_2])
 print(ca_gb_videos, '\n' * 10)
 
 
