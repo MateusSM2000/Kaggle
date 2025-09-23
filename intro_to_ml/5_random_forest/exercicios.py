a6f6d4be5119d5929bbccee5824d0f55c85e5944
MateusSM2000 <matt_petao@hotmail.com>
2025-09-23 11:37:02 -0300

    update

---
+++ intro_to_ml/5_random_forest/exercicios.py
@@ -0,0 +1,20 @@
+import pandas as pd
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_absolute_error
+from sklearn.model_selection import train_test_split
+
+
+iowa_data = pd.read_csv('../train.csv', index_col='Id')
+
+y = iowa_data.SalePrice
+features = ['LotArea', 'YearBuilt', '1stFlrSF', '2ndFlrSF', 'FullBath', 'BedroomAbvGr', 'TotRmsAbvGrd']
+X = iowa_data[features]
+
+train_X, val_X, train_y, val_y = train_test_split(X, y, random_state=1)
+
+forest_model = RandomForestRegressor(random_state=1)
+forest_model.fit(train_X, train_y)
+predictions = forest_model.predict(val_X)
+
+mae = mean_absolute_error(predictions, val_y)
+print(mae)
